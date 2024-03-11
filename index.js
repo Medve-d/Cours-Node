3206f3a8a3c6ad53043fb4597406d3f41d10c9d0
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-03-11 17:10:23 +0100

    exos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Exos fonctions 
 // fonction 1
 const Fonction1 = [1,2]
-console.log(Fonction1)
+console.log(Fonction1, "Fin de l'exercice 1")
 
 // Fonction 2
 
@@ -10,7 +10,7 @@ const ChiffresListe = [1,9,8,7,4,5,2,1,3,6,5,4,8,4,8,7];
 const PlusGrandChiffre = (chiffres) => (chiffres.length > 0 ? Math.max(...chiffres) : "La liste est vide.");
 
 const resultat = PlusGrandChiffre(ChiffresListe);
-console.log("Le plus grand chiffre est :", resultat);
+console.log("Le plus grand chiffre est :", resultat, "Fin de l'exercice 2");
 
 //fonction 3
  
@@ -19,7 +19,7 @@ const Motsavecvoyelle = "anticonstitutionnellement massi jordan mounir";
 const retirerVoyelles = (chaine) => chaine.replace(/[aeiouAEIOU]/g, '');
 
 const Sansvoyelles = retirerVoyelles(Motsavecvoyelle);
-console.log("Chaîne sans voyelles :", Sansvoyelles);
+console.log("Chaîne sans voyelles :", Sansvoyelles, "Fin de l'exercice 3");
 
 //fonction 4 
 
@@ -28,12 +28,10 @@ function trierLettresDansPhrase(phrase) {
     const phraseTriee = phrase.split('').sort().join('');
     return phraseTriee;
 }
-
-// Exemple d'utilisation de la fonction
 const MotATrier = "Anticonstitutionnellement";
 const phraseTriee = trierLettresDansPhrase(MotATrier);
 
-console.log("Phrase triée par ordre alphabétique :", phraseTriee);
+console.log("Phrase triée par ordre alphabétique :", phraseTriee, "Fin de l'éxercice 4");
 
 //fonction 5 
 
@@ -42,3 +40,10 @@ console.log("Phrase triée par ordre alphabétique :", phraseTriee);
 const Personne = ["Massi","Jordan","Mounir"]
 
 //fonction 7
+
+const Motsavecvoyelle2 = "anticonstitutionnellement massi jordan mounir";
+
+const AgrandirVoyelles = (chaine) => chaine.replace(/[aeiou]/g, '');
+
+const Grandevoyelles = AgrandirVoyelles(Motsavecvoyelle);
+console.log(chaine.toUpperCase(aeiou), Grandevoyelles, "Fin de l'exercice 3");
