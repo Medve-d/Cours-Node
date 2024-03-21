cfff6ada5bb5c9b77ae2e0329bf8c339a1d003e6
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-03-21 21:54:51 +0100

    recommencer à zéro

--- AppliWebNode/index.js
+++ AppliWebNode/index.js
@@ -1,24 +1,44 @@
-import express from "express";
+import Express from "express";
+import shopRouter from "./routes/shop.js";
 
-const app = express();
+const app = Express();
 
-app.use(express.static("public"));
+// Configure mon application pour qu'elle utlise ejs comme moteur de templating
+// l'outil qui va generer de l'html
+app.set("view engine", "ejs");
 
-app.set("view engine", "ejs")
+app.set("views", "views");
 
-app.set("views", "views")
+const middlware1 = (req, res, next) => {
+  console.log(
+    "Je suis un middleware qui s'execute a chaque requete envoyee au serveur"
+  );
+  req.toto = "toto";
+
+  next();
+};
+const middlware2 = (req, res, next) => {
+  console.log(
+    "Je suis un autre middleware qui s'execute a chaque requete envoyee au serveur"
+  );
+  req.toto = "toto3";
+
+  next();
+};
+
+app.use(middlware1, middlware2);
+
+app.use("/", (req, res, next) => {
+  console.log(req.toto);
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.render("home.ejs")
+  res.render("home", {});
 });
 
-app.get("/home", (req, res) => {
-  res.render("home.ejs")
-});
-app.get("/shop", (req, res) => {
-  res.render("shop.ejs")
-});
+app.use("/shop", shopRouter);
 
-
-// le serveur ecoute sur le port 3000 : http://localhost:3000
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
