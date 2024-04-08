f67aae343cc1a14fac0186b4597721edc0421c82
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-04-08 11:30:17 +0200

    aa

--- 08-04/src/index.js
+++ 08-04/src/index.js
@@ -1,1 +1,35 @@
-import Express from "express";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const mockCars = [
+    { id: 1, brand: "Audi", model: "A3" },
+    { id: 2, brand: "Renault", model: "Clio" },
+    { id: 3, brand: "Peugeot", model: "208" },
+    { id: 4, brand: "BMW", model: "Serie 1" },
+    { id: 5, brand: "Mercedes", model: "Classe A" },
+    { id: 6, brand: "Citroen", model: "C3" },
+    { id: 7, brand: "Ford", model: "Fiesta" },
+    { id: 8, brand: "Opel", model: "Corsa" },
+    { id: 9, brand: "Toyota", model: "Yaris" },
+    { id: 10, brand: "Volkswagen", model: "Polo" },
+  ];
+
+// Middlewares
+
+//middleware qui se charge de rediriger les req qui concernent les voitures vers le router des voitures
+//http://localhost:3001/cars
+app.use("/cars", (request, response) => {
+
+});
+
+// Routes définit une route GET / qui renvoie un message
+app.get("/", (request, response) => {
+    response.json({ message: "Hello World !"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
